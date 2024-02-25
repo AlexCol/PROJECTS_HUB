4a983320a8835597b651a7dc52eccf95e11cab28
Alexandre Coletti <eu_axil@yahoo.com.br>
2024-02-25 10:39:31 -0300

    adiciona informações da API de autenticação

--- src/components/Body/Body.tsx
+++ src/components/Body/Body.tsx
@@ -8,16 +8,13 @@ function Body() {
 		<div className={styles.body}>
 			<Meteor />
 			<div className={styles.cardList}>
-			<CardList title='Aplicações em React Puro.'>
+			<CardList title='Aplicações em React.'>
+				<MyCard header='React' title='Tela de Autenticação' message='Tela de autenticação que consome a Api de autenticação criada em aspnet, e realiza os direcionamentos devidos.' linkTo='https://www.facebook.com/'/>
 				<MyCard header='React' title='Jogo da Memória' message='Jogo da mémoria para testar e aprender funcionalidades.' linkTo='https://www.facebook.com/'/>
-				<MyCard header='algum header' title='algum title' message='Some message Some message Some message Some message Some message' linkTo='https://www.facebook.com/'/>
-				<MyCard header='algum header' title='algum title' message='Some message Some message Some message Some message Some message' linkTo='https://www.facebook.com/'/>
-				<MyCard header='algum header' title='algum title' message='Some message Some message Some message Some message Some message' linkTo='https://www.facebook.com/'/>
-				<MyCard header='algum header' title='algum title' message='Some message Some message Some message Some message Some message' linkTo='https://www.facebook.com/'/>
 			
 			</CardList>	
-			<CardList title='Aplicações React com AspNet.'>				
-				<MyCard header='algum header' title='algum title' message='Some message Some message Some message Some message Some message' linkTo='https://www.facebook.com/'/>
+			<CardList title='Aplicações em AspNet(C#).'>
+				<MyCard header='AspNet' title='Autenticador' message='Api de controle de Usuários - Login e Registro - para uso das demais aplicações.' linkTo='https://localhost:7119/swagger/index.html'/>
 			</CardList>
 			</div>
 		</div>
